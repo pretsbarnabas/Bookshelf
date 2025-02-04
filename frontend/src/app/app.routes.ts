a5e27cbcd851461f0c621b74c3012293e2571512
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2025-02-04 22:31:51 +0100

    Creating starter navbar, adding routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BooksComponent } from './components/books/books.component';
+import { SummariesComponent } from './components/summaries/summaries.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { MylistComponent } from './components/mylist/mylist.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BookItemComponent } from './components/book-item/book-item.component';
+import { SummaryItemComponent } from './components/summary-item/summary-item.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    // menubar routes
+    { path: 'books', component: BooksComponent},
+    { path: 'summaries', component: SummariesComponent},
+    { path: 'mylist', component: MylistComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'registration', component: RegistrationComponent},
+    { path: 'profile', component: ProfileComponent},
+    // non-visible routes
+    { path: 'book-item', component: BookItemComponent},
+    { path: 'summary-item', component: SummaryItemComponent},
+
+];
--- frontend/src/app/components/app/navbar/navbar.component.ts
+++ frontend/src/app/components/app/navbar/navbar.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { RouterModule } from '@angular/router';
+
 
 @Component({
-  selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+    selector: 'app-navbar',
+    imports: [
+        RouterModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        FlexLayoutModule,
+        MatSlideToggleModule,
+        MatButtonToggleModule
+    ],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss',
+    encapsulation: ViewEncapsulation.None,
 })
 export class NavbarComponent {
 
+    localizationToggleValue: string = "en";
+    isdarkModeOn = false;
+
+    changeTheme() {
+        this.isdarkModeOn = !this.isdarkModeOn;
+        document.body.classList.toggle('darkMode')
+    }
 }
