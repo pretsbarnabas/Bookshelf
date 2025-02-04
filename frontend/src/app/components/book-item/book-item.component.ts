2a44c6e7faf722ee1f315c094e9b609f172a679e
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2025-02-04 22:03:45 +0100

    Creating page components

---
+++ frontend/src/app/components/book-item/book-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-item',
+  imports: [],
+  templateUrl: './book-item.component.html',
+  styleUrl: './book-item.component.scss'
+})
+export class BookItemComponent {
+
+}
