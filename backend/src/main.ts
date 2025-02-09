dd098785d6d9c20a6a5fe91f53f184097378cd5d
pretsbarnabas <prets.barnabas@gmail.com>
2025-02-09 01:38:55 +0100

    #2 figured out how swagger works and tried it on a few routes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,8 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const routes = require("./routes.ts")
-
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express");
 const connectionString = process.env.DATABASE_URL
 
 if(!connectionString) throw new Error("No connection string defined")
@@ -19,9 +20,35 @@ database.once("connected",()=>{
     console.log("Database connected")
 })
 
+const options = {
+    definition: {
+      openapi: "3.1.0",
+      info: {
+        title: "Bookshelf Backend API",
+        version: "0.1.0",
+        description:
+          "The backend api route descriptions used by Bookshelf",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        }
+      },
+      servers: [
+        {
+          url: "http://localhost:3000/",
+        },
+      ],
+    },
+    apis: ["./src/routes.ts"],
+  };
+
+const specs = swaggerJsdoc(options)
+
 const app = express()
 app.use(express.json())
-app.use("/",routes)
+app.use("/api",routes)
+
+app.use("/api/swagger",swaggerUi.serve,swaggerUi.setup(specs))
 
 app.listen(3000,()=>{
     console.log("Server started")
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,11 +5,137 @@ import { ReviewController } from "./controllers/review.controller"
 import { CommentController } from "./controllers/comment.controller"
 import { SummaryController } from "./controllers/summary.controller"
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      user:
+ *          type: object
+ *          properties:
+ *              _id:
+ *                  type: object
+ *              username:
+ *                  type: string
+ *              password_hashed:
+ *                  type: string
+ *              email:
+ *                  type: string
+ *              created_at:
+ *                  type: string
+ *              updated_at:
+ *                  type: string
+ *              last_login:
+ *                  type: string
+ *              role:
+ *                  type: string
+ *              booklist:
+ *                  type: array
+ *          example:
+ *              _id: 67a5c3cba3bb48fa646f9638
+ *              username: User
+ *              password_hashed: $2b$10$Tk179TvlrUQqQuxU/XHoI.rP/edBea12duhNIOTV2VO9FZZhvRo5S
+ *              email: email@email.com
+ *              role: user
+ *              booklist: []
+ *              created_at: 2025-02-07T08:26:51.424Z
+ *              updated_at: 2025-02-07T08:26:51.424Z
+ *              last_login: 2025-02-07T08:26:51.424Z
+ *              
+ *  requestBodies:
+ *      user:
+ *          type: object
+ *          required: 
+ *              - username
+ *              - password
+ *              - email
+ *              - role
+ *          properties:
+ *              username:
+ *                  type: string
+ *              password:
+ *                  type: string
+ *              email:
+ *                  type: string
+ *              role:
+ *                  type: string
+ */
+
 export const router = express.Router()
 
+/**
+ * @swagger
+ * tags:
+ *   - name: login
+ *     description: Login feature
+ *
+ * /api/login:
+ *   post:
+ *     summary: Login for a user
+ *     tags: 
+ *       - login
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Verification token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ */
 router.post("/login",UserController.login)
 
 // User routes
+
+/**
+ * @swagger
+ * tags:
+ *   - name: Users
+ *     description: Routes managing users
+ *
+ * /api/users:
+ *   get:
+ *     summary: Get all users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The list of requested users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/user'
+ *
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/requestBodies/user'
+ *     responses:
+ *       200:
+ *         description: The created user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/user'
+ */
 router.get("/users/",UserController.getAllUsers)
 router.get("/users/:id",UserController.getUserById)
 router.post("/users/",UserController.createUser)
