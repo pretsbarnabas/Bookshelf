aba1bad6d98c15dd6ac064df7cfbd7ec3eb76fc2
[bence.pozsgai] <[bencepozsgai2005@gmail.com]>
2025-02-17 20:49:52 +0100

    Books Page started

--- frontend/src/app/components/pages/books/books.component.ts
+++ frontend/src/app/components/pages/books/books.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
+import { FormlyModule } from '@ngx-formly/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-books',
-  imports: [],
+  imports: [        
+    FormlyModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormlyMaterialModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule],
   templateUrl: './books.component.html',
   styleUrl: './books.component.scss'
 })
