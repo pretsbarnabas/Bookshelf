465198c184af37c3568a902dc81e24305e93292e
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2025-03-09 16:50:50 +0100

    https://music.youtube.com/watch?v=wVCGysjDdp8&list=RDAMVMwVCGysjDdp8

--- frontend/src/app/utilities/auth.guard.ts
+++ frontend/src/app/utilities/auth.guard.ts
@@ -1,17 +1,22 @@
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { map, of, switchMap } from 'rxjs';
 import { UserLoggedInModel } from '../models/User';
+import { isPlatformBrowser } from '@angular/common';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
     const authService = inject(AuthService);
     const router = inject(Router);
+    const platformId = inject(PLATFORM_ID);
 
     let userRole: string | null = null;
-    if(localStorage.getItem('authToken'))
-        userRole = authService.decodeToken().role;
+
+    if (isPlatformBrowser(platformId)) {
+        if (localStorage.getItem('authToken'))
+            userRole = authService.decodeToken().role;
+    }
 
     if (state.url.startsWith('/auth')) {
         if (!userRole) {
