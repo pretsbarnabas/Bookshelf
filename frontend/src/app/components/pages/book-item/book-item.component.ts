1611a4de7ed01c41bccc85e3c8904aa5f77b388b
Pozsgai Bence <pozsgai.bence@students.jedlik.eu>
2025-03-11 12:53:38 +0100

    implemented an angular material based star rating system it does not work fully but i am working on it #6

--- frontend/src/app/components/pages/book-item/book-item.component.ts
+++ frontend/src/app/components/pages/book-item/book-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookService } from '../../../services/book.service';
 import { MatCardModule } from '@angular/material/card';
@@ -10,7 +10,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule, DatePipe } from '@angular/common';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-book-item',
   templateUrl: './book-item.component.html',
@@ -30,20 +30,54 @@ import { CommonModule, DatePipe } from '@angular/common';
 })
 export class BookItemComponent implements OnInit {
   @Input() book: any;
+  @Input('rating') public rating: number = 3;
+  @Input('starCount') public starCount: number = 5;
+  @Input('color') public color: string = 'accent';
+  @Output() private ratingUpdated = new EventEmitter();
+
   bookId: any;
+  private snackBarDuration: number = 2000;
+  public ratingArr: any = [];
 
-  constructor(private route: ActivatedRoute, private bookService: BookService, private datePipe: DatePipe) { }
+  constructor(private route: ActivatedRoute, private bookService: BookService, private datePipe: DatePipe, private snackBar: MatSnackBar){}
 
-  ngOnInit(): void {
+  ngOnInit(){
     this.bookId = this.route.snapshot.paramMap.get('id');
     if (this.bookId) {
       this.bookService.getBookById(this.bookId).subscribe(book => {
         this.book = book;
       });
     }
+    for (let index = 0; index < this.starCount; index++) {
+      this.ratingArr.push(index);
+    }
   }
   
   formatDate(date: any) {
     return this.datePipe.transform(date, 'yyyy');
   }
+  onClick(rating:number) {
+    // console.log(rating)
+    this.snackBar.open('You rated ' + rating + ' / ' + this.starCount, '', {
+      duration: this.snackBarDuration
+    });
+    this.ratingUpdated.emit(rating);
+    return false;
+  }
+
+  showIcon(index:number) {
+    console.log(index)
+    if (this.rating >= index + 1) {
+      return 'star';
+    } else {
+      return 'star_border';
+    }
+  }
+
 }
+  export enum StarRatingColor {
+    primary = "primary",
+    accent = "accent",
+    warn = "warn"
+  }
+
