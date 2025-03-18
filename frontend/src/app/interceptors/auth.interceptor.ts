521411c16297e4c3e9301623aa703a5325ebdda3
boka matyas <boka.matyas@students.jedlik.eu>
2025-03-18 11:56:49 +0100

    #15 menu plasterfix

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -2,10 +2,12 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { jwtDecode } from 'jwt-decode';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/global/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const token = localStorage.getItem('authToken');
     const router = inject(Router);
+    const authService = inject(AuthService);
 
     if (token) {
         try {
@@ -13,7 +15,6 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
             const currentTime = Math.floor(Date.now() / 1000);
 
             if (decoded.exp && decoded.exp < currentTime) {
-                console.warn('Token expired, logging out...');
                 localStorage.removeItem('authToken');
                 router.navigate(['auth/login']);
                 return next(req);
@@ -25,8 +26,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
             return next(clonedRequest);
         } catch (error) {
-            console.error('Invalid token:', error);
-            localStorage.removeItem('authToken');
+            authService.logOut();
             router.navigate(['auth/login']);
             return next(req);
         }
