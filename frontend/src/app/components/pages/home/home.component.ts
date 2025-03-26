231fd4fbd74c195a7dc6132a9352b4e11fa5aedc
[bence.pozsgai] <[bencepozsgai2005@gmail.com]>
2025-03-27 00:19:26 +0100

    Finally everything works I can finally  sleep

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -5,26 +5,38 @@ import { Book } from '../../../models/Book';
 import { MatCardModule } from '@angular/material/card';
 import { Router } from '@angular/router';
 import { CommonModule, DatePipe } from '@angular/common';
+import { AuthService } from '../../../services/global/auth.service';
+import { UserModel } from '../../../models/User';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [TranslatePipe, MatCardModule, CommonModule],
+  imports: [TranslatePipe, MatCardModule, CommonModule, MatButtonModule, MatIconModule],
   providers: [DatePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent {
-  constructor(private bookService: BookService,private router:Router, private datePipe: DatePipe) { }
+  constructor(private bookService: BookService,private router:Router, private datePipe: DatePipe, private authService:  AuthService ) { }
   pageSize = 10;
   currentPageIndex = 0;
   maxPages = 0;
   books: Book[] = [];
+  isLoggedIn: boolean = false;
+  loggedInUser: UserModel | null = null;
 
   ngOnInit(): void {
     this.getBooks();
+    this.authService.loggedInUser$.subscribe(user => {
+      this.isLoggedIn = !!user;
+      this.loggedInUser = user;
+    });
    }
-
+   navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
   getBooks(): void {
     this.bookService.getAllBooks(this.pageSize, this.currentPageIndex).subscribe({
       next: (data) => {
