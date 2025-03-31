b66c95ba904f68502ef2c4551cec2221dc77a4f6
boka matyas <boka.matyas@students.jedlik.eu>
2025-03-31 09:00:43 +0200

    Add sort-items.component tests

--- frontend/src/tests/components/sort-items.component.spec.ts
+++ frontend/src/tests/components/sort-items.component.spec.ts
@@ -19,14 +19,35 @@ describe('SortItemsComponent', () => {
 
         fixture = TestBed.createComponent(SortItemsComponent);
         component = fixture.componentInstance;
-        component.type = 'user';
-        component.isAdmin = false;
         fixture.detectChanges();
     });
-
+    
     it('Should create the component', () => {
         expect(component).toBeTruthy();
     });
-
     
+    it('Should get the correct field mapping', () =>{
+        spyOn(SortItemsComponent.prototype, 'ngOnChanges').and.callThrough();        
+        component.type = 'summary';
+        component.isAdmin = false;
+        component.ngOnChanges();        
+        expect(component.sortedFields).toEqual(['updated_at',])
+
+        
+        component.type = 'review';
+        component.isAdmin = true;
+        component.ngOnChanges();
+        expect(component.sortedFields).toEqual(['_id', 'user._id', 'score', 'created_at', 'updated_at'])
+    });
+
+    it('Should emit selected field', ()=>{
+        spyOn(SortItemsComponent.prototype, 'emitChangedValue').and.callThrough();
+        spyOn(component.onSortingChanged, 'emit');
+        component.selectedField = '_id';
+        component.mode = 'asc';
+
+        component.emitChangedValue();
+        expect(component.onSortingChanged.emit).toHaveBeenCalledWith({field: '_id', mode: 'asc'});
+        
+    });
 });
