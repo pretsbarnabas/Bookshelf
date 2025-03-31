2cdcb5fe836bcb98bb3299c576c325e6cd620ba8
boka matyas <boka.matyas@students.jedlik.eu>
2025-03-31 08:37:33 +0200

    Add router-button.component test

--- frontend/src/tests/components/router-button.component.spec.ts
+++ frontend/src/tests/components/router-button.component.spec.ts
@@ -1,23 +1,51 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterButtonComponent } from '../../app/components/layout/router-button/router-button.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NavigationEnd, Router } from '@angular/router';
 
-// import { RouterButtonComponent } from './router-button.component';
 
-// describe('RouterButtonComponent', () => {
-//   let component: RouterButtonComponent;
-//   let fixture: ComponentFixture<RouterButtonComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [RouterButtonComponent]
-//     })
-//     .compileComponents();
+import { Subject } from 'rxjs';
 
-//     fixture = TestBed.createComponent(RouterButtonComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+describe('RouterButtonComponent', () => {
+    let component: RouterButtonComponent;
+    let router: Router;
+    const routerEventsSubject = new Subject();
+    let fixture: ComponentFixture<RouterButtonComponent>;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule.withRoutes([]),
+                RouterButtonComponent,
+                TranslateModule.forRoot(),
+            ],
+            providers: [
+                {
+                    provide: Router, useValue: {
+                        events: routerEventsSubject.asObservable(),
+                        routerState: { root: {} },
+                        url: '/',
+                        navigate: jasmine.createSpy('navigate')
+                    }
+                },
+            ]
+        }).compileComponents();
+
+        router = TestBed.inject(Router);
+        fixture = TestBed.createComponent(RouterButtonComponent);
+        component = fixture.componentInstance;
+        component.payload = { route: '/test', icon: 'test_icon', localReference: 'APP.TEST' };
+        fixture.detectChanges();
+    });
+
+    it('Should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('Should set activated route on navigation properly', () => {
+        routerEventsSubject.next(new NavigationEnd(1, '/test', '/test'));
+        expect(component.activeRoute).toBe('/test');
+    })
+});
