982043d29807c40d8dde60be12d2d13d81298e4f
[bence.pozsgai] <[bencepozsgai2005@gmail.com]>
2025-03-31 21:12:51 +0200

    Started Mylist component #30

--- frontend/src/app/components/pages/mylist/mylist.component.ts
+++ frontend/src/app/components/pages/mylist/mylist.component.ts
@@ -1,12 +1,33 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mylist',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatTabsModule
+  ],
   templateUrl: './mylist.component.html',
-  styleUrl: './mylist.component.scss',
+  styleUrls: ['./mylist.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
 export class MylistComponent {
+  toReadBooks = [
+    { title: 'Book 1', author: 'Author 1', image: 'https://via.placeholder.com/150', description: 'Description 1' },
+    { title: 'Book 2', author: 'Author 2', image: 'https://via.placeholder.com/150', description: 'Description 2' },
+  ];
 
+  readingBooks = [
+    { title: 'Book 3', author: 'Author 3', image: 'https://via.placeholder.com/150', description: 'Description 3' },
+    { title: 'Book 4', author: 'Author 4', image: 'https://via.placeholder.com/150', description: 'Description 4' },
+  ];
+
+  stoppedReadingBooks = [
+    { title: 'Book 5', author: 'Author 5', image: 'https://via.placeholder.com/150', description: 'Description 5' },
+    { title: 'Book 6', author: 'Author 6', image: 'https://via.placeholder.com/150', description: 'Description 6' },
+  ];
 }
