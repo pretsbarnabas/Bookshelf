e1d96cce7f2cd9600b9e340f86790a02f8a982eb
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2025-04-01 22:46:33 +0200

    Remove console.log from formatRemainingTime

--- frontend/src/app/utilities/formatRemainingTime.ts
+++ frontend/src/app/utilities/formatRemainingTime.ts
@@ -4,7 +4,6 @@ export function formatRemainingTime(_remainingTime: number, lang: string): strin
     const days = Math.floor(_remainingTime / (1000 * 60 * 60 * 24));
     const hours = String(Math.floor((_remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, '0');
     const minutes = String(Math.floor((_remainingTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
-    console.log(days)
 
     return days >= 1 ? `${days} ${lang === 'en' ? (days === 1 ? 'day' : 'days') : 'nap'}` : `${hours} : ${minutes}`;
 }
