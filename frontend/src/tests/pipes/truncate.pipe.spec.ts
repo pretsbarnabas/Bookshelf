b2b8c83742d116dc400556132ca78d28af0a1478
boka matyas <boka.matyas@students.jedlik.eu>
2025-04-01 08:52:04 +0200

    Add truncate.pipe tests

--- frontend/src/tests/pipes/truncate.pipe.spec.ts
+++ frontend/src/tests/pipes/truncate.pipe.spec.ts
@@ -1,8 +1,29 @@
-// import { TruncatePipe } from './truncate.pipe';
+import { pipe } from "rxjs";
+import { TruncatePipe } from "../../app/pipes/truncate.pipe";
+import { TestBed } from "@angular/core/testing";
 
-// describe('TruncatePipe', () => {
-//   it('create an instance', () => {
-//     const pipe = new TruncatePipe();
-//     expect(pipe).toBeTruthy();
-//   });
-// });
+
+describe('TruncatePipe', () => {
+    let pipe: TruncatePipe;
+
+    beforeEach(async () => {
+        TestBed.configureTestingModule({
+            providers: [
+                TruncatePipe
+            ]
+        });
+        pipe = TestBed.inject(TruncatePipe);
+    })
+
+    it('Create an instance', () => {
+        expect(pipe).toBeTruthy();
+    });
+
+    it('Should truncate text of 8 characters or longer', () => {
+        expect(pipe.transform('longtextfortesting')).toBe('longtext...');
+    })
+
+    it('Should not truncate text less than 8 characters', () => {
+        expect(pipe.transform('short')).toBe('short');
+    })
+});
