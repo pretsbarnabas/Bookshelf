5b262bab7b62f342498a0d96fef2abb7afe65035
Pozsgai Bence@ <pozsgai.bence@students.jedlik.eu>
2025-04-01 13:53:05 +0200

    added some styling to the page #30

--- frontend/src/app/components/pages/mylist/mylist.component.ts
+++ frontend/src/app/components/pages/mylist/mylist.component.ts
@@ -17,17 +17,17 @@ import { CommonModule } from '@angular/common';
 })
 export class MylistComponent {
   toReadBooks = [
-    { title: 'Book 1', author: 'Author 1', image: 'https://via.placeholder.com/150', description: 'Description 1' },
-    { title: 'Book 2', author: 'Author 2', image: 'https://via.placeholder.com/150', description: 'Description 2' },
+    { title: 'Book 1', author: 'Author 1', image: 'assets/images/bingus.webp'},
+    { title: 'Book 2', author: 'Author 2', image: 'assets/images/bingus.webp'},
   ];
 
   readingBooks = [
-    { title: 'Book 3', author: 'Author 3', image: 'https://via.placeholder.com/150', description: 'Description 3' },
-    { title: 'Book 4', author: 'Author 4', image: 'https://via.placeholder.com/150', description: 'Description 4' },
+    { title: 'Book 3', author: 'Author 3', image: 'assets/images/bingus.webp'},
+    { title: 'Book 4', author: 'Author 4', image: 'assets/images/bingus.webp'},
   ];
 
   stoppedReadingBooks = [
-    { title: 'Book 5', author: 'Author 5', image: 'https://via.placeholder.com/150', description: 'Description 5' },
-    { title: 'Book 6', author: 'Author 6', image: 'https://via.placeholder.com/150', description: 'Description 6' },
+    { title: 'Book 5', author: 'Author 5', image: 'assets/images/bingus.webp'},
+    { title: 'Book 6', author: 'Author 6', image: 'assets/images/bingus.webp'},
   ];
 }
