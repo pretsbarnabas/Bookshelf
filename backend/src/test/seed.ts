f5c1ae52ef31ed65bd7398f0fd6e4a8e0eba423d
Préts Barnabás <prets.barnabas@gmail.com>
2025-04-02 11:27:15 +0200

    ű

--- backend/src/test/seed.ts
+++ backend/src/test/seed.ts
@@ -1,32 +1,32 @@
 const MongoClient = require("mongodb").MongoClient
-const Types = require("mongoose").Types
+import mongoose from "mongoose"
 
 
 
 async function seed(){
-    const uri = "mongodb://localhost:27017"
+    const uri = "mongodb://localhost:27017/Bookshelf"
 
-    const client = new MongoClient(uri,{})
+    await mongoose.connect(uri)
+    const client = mongoose.connection.db!
 
     try {
-        await client.connect()
         console.log("Seeder: Connected to db")
 
-        const usersCollection = client.db("Bookshelf").collection("users")
-        const booksCollection = client.db("Bookshelf").collection("books")
-        const reviewsCollection = client.db("Bookshelf").collection("reviews")
-        const commentsCollection = client.db("Bookshelf").collection("comments")
-        const summariesCollection = client.db("Bookshelf").collection("summaries")
+        const usersCollection = client.collection("users")
+        const booksCollection = client.collection("books")
+        const reviewsCollection = client.collection("reviews")
+        const commentsCollection = client.collection("comments")
+        const summariesCollection = client.collection("summaries")
 
-        usersCollection.drop()
-        booksCollection.drop()
-        reviewsCollection.drop()
-        commentsCollection.drop()
-        summariesCollection.drop()
 
+        const collections = (await client.listCollections().toArray()).map((collection:any) => collection.name as string);
+
+        for (let i = 0; i < collections.length; i++) {
+            await client.dropCollection(collections[i]);
+        }
 
         await usersCollection.insertOne({
-            "_id": new Types.ObjectId("db0b0c1f83fb29f652cc5a2f"),
+            "_id": new mongoose.Types.ObjectId("db0b0c1f83fb29f652cc5a2f"),
             "username": "admin",
             "password_hashed": "$2b$10$2ticgdLvmAs4b5epdj3rZeyf1.04UBUbxR7m/kk/HAYhhGVLAort.",
             "email": "admin@email.com",
@@ -36,28 +36,28 @@ async function seed(){
             "role": "admin",
             "booklist": [
                 {
-                    "book_id": new Types.ObjectId("7fdb24bfd2c9eaca400201b8"),
+                    "book_id": new mongoose.Types.ObjectId("7fdb24bfd2c9eaca400201b8"),
                     "read_status": "to_read"
                 }
             ]
         })
 
         await booksCollection.insertOne({
-            "_id": new Types.ObjectId("7fdb24bfd2c9eaca400201b8"),
+            "_id": new mongoose.Types.ObjectId("7fdb24bfd2c9eaca400201b8"),
             "title": "Lorem",
             "author": "Lorem",
             "release": new Date("2016-04-08T15:06:21.595Z"),
             "genre": "Crime",
-            "user_id": new Types.ObjectId("db0b0c1f83fb29f652cc5a2f"),
+            "user_id": new mongoose.Types.ObjectId("db0b0c1f83fb29f652cc5a2f"),
             "description": "Lorem",
             "added_at": new Date("2016-04-08T15:06:21.595Z"),
             "updated_at": new Date("2016-04-08T15:06:21.595Z")
         })
 
         await reviewsCollection.insertOne({
-            "_id": new Types.ObjectId("a826b9febba8c411cf6d82cb"),
-            "user_id": new Types.ObjectId("db0b0c1f83fb29f652cc5a2f"),
-            "book_id": new Types.ObjectId("7fdb24bfd2c9eaca400201b8"),
+            "_id": new mongoose.Types.ObjectId("a826b9febba8c411cf6d82cb"),
+            "user_id": new mongoose.Types.ObjectId("db0b0c1f83fb29f652cc5a2f"),
+            "book_id": new mongoose.Types.ObjectId("7fdb24bfd2c9eaca400201b8"),
             "score": 8,
             "content": "Lorem",
             "created_at": new Date("2016-04-08T15:06:21.595Z"),
@@ -65,25 +65,24 @@ async function seed(){
         })
 
         await commentsCollection.insertOne({
-            "_id": new Types.ObjectId("7ef503b3e63a6bcb56f8db37"),
-            "user_id": new Types.ObjectId("db0b0c1f83fb29f652cc5a2f"),
-            "review_id": new Types.ObjectId("a826b9febba8c411cf6d82cb"),
+            "_id": new mongoose.Types.ObjectId("7ef503b3e63a6bcb56f8db37"),
+            "user_id": new mongoose.Types.ObjectId("db0b0c1f83fb29f652cc5a2f"),
+            "review_id": new mongoose.Types.ObjectId("a826b9febba8c411cf6d82cb"),
             "content": "Lorem",
             "created_at": new Date("2016-04-08T15:06:21.595Z"),
             "updated_at": new Date("2016-04-08T15:06:21.595Z")
         })
 
         await summariesCollection.insertOne({
-            "_id": new Types.ObjectId("72bd5cfdf84d7fea96483da4"),
-            "user_id": new Types.ObjectId("db0b0c1f83fb29f652cc5a2f"),
-            "book_id": new Types.ObjectId("7fdb24bfd2c9eaca400201b8"),
+            "_id": new mongoose.Types.ObjectId("72bd5cfdf84d7fea96483da4"),
+            "user_id": new mongoose.Types.ObjectId("db0b0c1f83fb29f652cc5a2f"),
+            "book_id": new mongoose.Types.ObjectId("7fdb24bfd2c9eaca400201b8"),
             "content": "Lorem",
             "created_at": new Date("2016-04-08T15:06:21.595Z"),
             "updated_at": new Date("2016-04-08T15:06:21.595Z")
         })
 
         console.log("Seeder: Seeded")
-        client.close()
     } catch (error) {
         console.log(error)
     }
