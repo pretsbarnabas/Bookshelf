2f781b465ea7465b6da2c7804e3b3fd6b0b4d5e4
boka matyas <boka.matyas@students.jedlik.eu>
2025-04-02 08:17:06 +0200

    Add admin.component tests

--- frontend/src/tests/pages/admin.component.spec.ts
+++ frontend/src/tests/pages/admin.component.spec.ts
@@ -4,6 +4,8 @@ import { CrudService } from '../../app/services/global/crud.service';
 import { provideHttpClient } from '@angular/common/http';
 import { provideConfig } from '../../app/services/global/config.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { By } from '@angular/platform-browser';
+import { AllTypeDisplayComponent } from '../../app/utilities/components/all-type-display/all-type-display.component';
 
 describe('AdminComponent', () => {
     let component: AdminComponent;
@@ -30,4 +32,11 @@ describe('AdminComponent', () => {
     it('Should create the component', () => {
         expect(component).toBeTruthy();
     });
+
+    it('Should have called all-type-display component in the html', ()=>{        
+        expect(fixture.nativeElement.querySelector('all-type-display')).toBeDefined();
+        
+        const childComponent = fixture.debugElement.query(By.directive(AllTypeDisplayComponent)).componentInstance;
+        expect(childComponent.isAdmin).toBeTrue();
+    })
 });
