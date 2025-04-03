ccf68e9ca2beb1b37525fa8e36b862b365970648
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2025-04-04 00:08:06 +0200

    Funny commit message no.2531

---
+++ frontend/src/tests/components/review-display.component.spec.ts
@@ -0,0 +1,23 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { ReviewDisplayComponent } from './review-display.component';
+
+// describe('ReviewDisplayComponent', () => {
+//     let component: ReviewDisplayComponent;
+//     let fixture: ComponentFixture<ReviewDisplayComponent>;
+
+//     beforeEach(async () => {
+//         await TestBed.configureTestingModule({
+//             imports: [ReviewDisplayComponent]
+//         })
+//             .compileComponents();
+
+//         fixture = TestBed.createComponent(ReviewDisplayComponent);
+//         component = fixture.componentInstance;
+//         fixture.detectChanges();
+//     });
+
+//     it('should create', () => {
+//         expect(component).toBeTruthy();
+//     });
+// });
