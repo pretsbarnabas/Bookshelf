0d7e763b5e94321f126c910c361fb4322d26c163
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2025-04-06 19:32:41 +0200

    Expanding summaries.component.spec testbed

--- frontend/src/tests/pages/summaries.component.spec.ts
+++ frontend/src/tests/pages/summaries.component.spec.ts
@@ -1,5 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SummariesComponent } from '../../app/components/pages/summaries/summaries.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideConfig } from '../../app/services/global/config.service';
+import { CrudService } from '../../app/services/global/crud.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('SummariesComponent', () => {
     let component: SummariesComponent;
@@ -8,7 +14,20 @@ describe('SummariesComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [
-                SummariesComponent
+                SummariesComponent,
+                TranslateModule.forRoot()
+            ],
+            providers: [
+                provideHttpClient(),
+                provideConfig(['apiurl', 'https://testing.com']),
+                CrudService,
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: { paramMap: { get: () => '/summary-item' } },
+                        queryParams: of({ test: 'testValue' }),
+                    },
+                },
             ]
         }).compileComponents();
 
