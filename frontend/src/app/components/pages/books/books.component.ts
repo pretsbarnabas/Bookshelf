547a18f63a361fd056efae30606835abed0d78e2
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2025-04-06 19:30:32 +0200

    Add book-display.component, Implemented on books and summaries page

--- frontend/src/app/components/pages/books/books.component.ts
+++ frontend/src/app/components/pages/books/books.component.ts
@@ -1,137 +1,16 @@
-import { Component, Renderer2, ElementRef, ViewChild, OnInit, ViewEncapsulation, inject } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
-import { FormlyModule } from '@ngx-formly/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormlyMaterialModule } from '@ngx-formly/material';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { BookService } from '../../../services/page/book.service';
-import { CommonModule, DatePipe } from '@angular/common';
-
-import { FormsModule } from '@angular/forms';
-import { BookModel } from '../../../models/Book';
-
-import { CustomPaginatorComponent } from '../../../utilities/components/custom-paginator/custom-paginator.component';
-import { TranslatePipe } from '@ngx-translate/core';
-import { SortItems } from '../../../utilities/components/sort-items';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MediaChange, MediaObserver } from '@angular/flex-layout';
-import { map } from 'rxjs/internal/operators/map';
-import { UserModel } from '../../../models/User';
-import { AuthService } from '../../../services/global/auth.service';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { BookDisplayComponent } from '../../../utilities/components/book-display/book-display.component';
 
 @Component({
     selector: 'app-books',
     standalone: true,
     imports: [
-        FormlyModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatInputModule,
-        FormlyMaterialModule,
-        ReactiveFormsModule,
-        MatButtonModule,
-        MatIconModule,
-        CommonModule,
-        CustomPaginatorComponent,
-        FormsModule,
-        TranslatePipe,
-        MatDividerModule,
-        RouterModule,
-        MatTooltipModule
+        BookDisplayComponent
     ],
     templateUrl: './books.component.html',
     styleUrls: ['./books.component.scss'],
-    providers: [DatePipe],
     encapsulation: ViewEncapsulation.None,
 })
-export class BooksComponent implements OnInit {
-    private mediaObserver = inject(MediaObserver);
-    private authService = inject(AuthService);
+export class BooksComponent {
 
-    @ViewChild('container') container!: ElementRef;
-    @ViewChild('showMoreButton') showMoreButton!: ElementRef;
-
-    isMdOrBeyond: boolean = false;
-    user: UserModel | null = null;
-
-    maxPages: number = 0;
-    currentPageIndex = 0;
-    pageSize = 10;
-
-    books: BookModel[] = [];
-    filteredBooks: BookModel[] = [];
-    searchTerm: string = '';
-
-    constructor(private bookService: BookService, private datePipe: DatePipe, private router: Router) { }
-
-    ngOnInit() {
-        this.getBooks();
-        this.mediaObserver.asObservable().pipe(
-            map((changes: MediaChange[]) => {
-                const isMdOrBeyond = changes.some(change => ['md', 'lg', 'xl'].includes(change.mqAlias));
-                this.isMdOrBeyond = isMdOrBeyond;
-            })
-        ).subscribe();
-        this.authService.loggedInUser$.subscribe((user) => {
-            this.user = user;
-        });
-    }
-
-    filterBooks(): void {
-        this.filteredBooks = this.books.filter(book =>
-            book.title.toLowerCase().startsWith(this.searchTerm.toLowerCase())
-        );
-    }
-
-    getBooks() {
-        this.bookService.getAllBooks(this.pageSize, this.currentPageIndex).subscribe({
-            next: (data) => {
-                this.books = data.data;
-                this.filteredBooks = this.books;
-                this.maxPages = data.pages;
-            },
-            error: (err) => {
-                console.error('Error fetching books', err);
-            }
-        });
-    }
-
-    changePage(changes: { pageIndex: number; pageSize: number }) {
-        this.currentPageIndex = changes.pageIndex;
-        this.pageSize = changes.pageSize;
-        this.getBooks();
-    }
-
-    navigateToBook(bookId: string) {
-        this.router.navigate(['/book-item', bookId]);
-    }
-
-    formatDate(date: any) {
-        return this.datePipe.transform(date, 'yyyy');
-    }
-
-    sortItems(_settings: { field: string, mode: 'asc' | 'desc' }): void {
-        if (_settings.field === '') {
-            this.filteredBooks = structuredClone(this.books);
-            return;
-        }
-        this.filteredBooks = structuredClone(this.books);
-        this.filteredBooks = SortItems.generalizedSort(this.filteredBooks as any[], _settings.field, _settings.mode);
-    }
-
-    // sortBooks(property: keyof BookModel, order: 'asc' | 'desc') {
-    //     this.filteredBooks.sort((a, b) => {
-    //         const comparison = (a[property] as string).localeCompare(b[property] as string);
-    //         return order === 'asc' ? comparison : -comparison;
-    //     });
-    // }
-
-    navigateToCreate() {
-        this.router.navigate(['/create/book']);
-    }
 }
--- frontend/src/app/components/pages/summaries/summaries.component.ts
+++ frontend/src/app/components/pages/summaries/summaries.component.ts
@@ -1,8 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { BookDisplayComponent } from "../../../utilities/components/book-display/book-display.component";
 
 @Component({
   selector: 'app-summaries',
-  imports: [],
+  imports: [BookDisplayComponent],
   templateUrl: './summaries.component.html',
   styleUrl: './summaries.component.scss',
   encapsulation: ViewEncapsulation.None,
